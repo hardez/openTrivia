4adc912b6cc4109640bed7ac0062ccbbb3695835
Marco Eckhardt <marco.eckhardt@me.com>
2018-10-15 10:23:02 +0200

    init + Questionmodel

---
+++ OpenTrivia/Question.swift
@@ -0,0 +1,31 @@
+//
+//  Question.swift
+//  OpenTrivia
+//
+//  Created by Marco Eckhardt on 14.10.18.
+//  Copyright © 2018 Marco Eckhardt. All rights reserved.
+//
+
+import Foundation
+
+enum Difficulty:String, Decodable{
+    case easy, medium, hard
+}
+
+enum Type:String, Decodable{
+    case multiple, boolean
+}
+
+struct Question: Decodable{
+    let category: String
+    let type: Type
+    let difficulty: Difficulty
+    let question: String
+    let correct_answer: String
+    let incorrect_answers: [String]
+}
+
+struct response: Decodable{
+    let response_code: Int
+    let results: [Question]
+}
